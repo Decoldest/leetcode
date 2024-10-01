25fd83a97d0dc119a5b645d16ada5eeb3e4a0a8d
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-10-01 15:30:44 -0400

    two integer sum II solution

--- two_pointers/two_pointers.py
+++ two_pointers/two_pointers.py
@@ -20,4 +20,16 @@ class Solution:
         
         return True
     
-    
+# Two Integer Sum II
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        start, end = 0, len(numbers) - 1
+
+        while start < end:
+            current = numbers[start] + numbers[end]
+            if current > target:
+                end -= 1
+            elif current < target:
+                start += 1
+            else:
+                return [start + 1, end + 1]
