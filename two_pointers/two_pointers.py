0675ea20aa88930b5e4f4ae261ff7142561ea378
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-10-01 17:22:43 -0400

    three integer sum solution

--- two_pointers/two_pointers.py
+++ two_pointers/two_pointers.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # Is Palindrome
 class Solution:
     def isPalindrome(self, s: str) -> bool:
@@ -33,3 +35,33 @@ class Solution:
                 start += 1
             else:
                 return [start + 1, end + 1]
+            
+# Three Integer Sum
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+
+        for index, current in enumerate(nums):
+            if current > 0:
+                break
+            if index > 0 and current == nums[index - 1]:
+                continue
+
+            middle, third = index + 1, len(nums) - 1
+
+            while middle < third:
+                numSum = current + nums[middle] + nums[third]
+                if numSum > 0:
+                    third -= 1
+                elif numSum < 0:
+                    middle += 1
+                else:
+                    result.append([current, nums[middle], nums[third]])
+                    middle += 1
+                    third -= 1
+                    while nums[middle] == nums[middle - 1] and middle < third:
+                        middle += 1
+
+
+        return result
