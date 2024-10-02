eec7d4c137e1533230977c64a6dc14a08e23c45f
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-10-02 14:50:04 -0400

    trapping rain water solution

--- two_pointers/two_pointers.py
+++ two_pointers/two_pointers.py
@@ -85,4 +85,26 @@ class Solution:
 
         return result
 
+# Trapping Rain Water
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        result = 0
+
+        if not height:
+            return result
         
+        left, right = 0, len(height) - 1
+        leftMax, rightMax = height[left], height[right]
+
+        while left < right:
+            if leftMax < rightMax:
+                left += 1
+                leftMax = max(leftMax, height[left])
+                result += leftMax - height[left]
+            else:
+                right -= 1
+                rightMax = max(rightMax, height[right])
+                result += rightMax - height[right]
+
+        return result
+            
