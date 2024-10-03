4740940e55c01da6364bec8d264544866fefb29d
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-10-03 17:25:01 -0400

    longest substring without duplicates solution

--- sliding_window/sliding_window.py
+++ sliding_window/sliding_window.py
@@ -1,4 +1,6 @@
-# Buy and Sell Crypto
+from typing import List
+
+# Buy and Sell Stock
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         result = 0
@@ -10,4 +12,21 @@ class Solution:
                 min = curr
             result = max(result, curr - min)
         
+        return result
+    
+# Longest Substring Witohut Duplicates
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        result, start = 0, 0
+        currentSet = set()
+
+        for r in range(len(s)):
+            letter = s[r]
+
+            while letter in currentSet:
+                currentSet.remove(s[start])
+                start += 1
+
+            currentSet.add(letter)
+            result = max(result, r - start + 1)
         return result
