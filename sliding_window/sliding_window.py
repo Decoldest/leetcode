9a8e6ef240d113725ac7af34e9da3c3ff6bf58a7
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-10-07 17:41:43 -0400

    longest repeating substring with replacement

--- sliding_window/sliding_window.py
+++ sliding_window/sliding_window.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import List
 
 # Buy and Sell Stock
@@ -14,7 +15,7 @@ class Solution:
         
         return result
     
-# Longest Substring Witohut Duplicates
+# Longest Substring Without Duplicates
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         result, start = 0, 0
@@ -29,4 +30,22 @@ class Solution:
 
             currentSet.add(letter)
             result = max(result, r - start + 1)
+        return result
+    
+
+# Longest Repeating Substring With Replacement
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = defaultdict(int)
+        left, result = 0, 0
+
+        for i in range(len(s)):
+            count[s[i]] += 1
+
+            while(i - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
+
+            result = max(result, i - left + 1)
+
         return result
